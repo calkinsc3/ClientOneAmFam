e5c9517612b226324e5416bd66d05549b68100a6
Levi Lavender <lsl017@amfam.com>
2015-07-16 11:25:50 -0500

    Splash screen built, Policy Screen Layout built, building card views

---
+++ app/src/main/java/com/example/jaz020/clientoneamfam/PolicyScreenFragment.java
@@ -0,0 +1,34 @@
+package com.example.jaz020.clientoneamfam;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PolicyScreenFragment extends Fragment {
+
+
+    public PolicyScreenFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_policy_screen, container, false);
+
+
+
+        return view;
+    }
+
+
+}
