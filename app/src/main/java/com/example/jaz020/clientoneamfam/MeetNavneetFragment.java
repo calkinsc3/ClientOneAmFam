d97d3097b4ed127b5be8d2cdbb2afc6d2e29a2f4
Navneet Reddy <nsr009@amfam.com>
2015-07-20 09:31:34 -0500

    Meet Navneet Fragment - picture, name, and titles done.

--- app/src/main/java/com/example/jaz020/clientoneamfam/MeetNavneetFragment.java
+++ app/src/main/java/com/example/jaz020/clientoneamfam/MeetNavneetFragment.java
@@ -1,6 +1,5 @@
 package com.example.jaz020.clientoneamfam;
 
-
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,13 +12,17 @@ import android.view.ViewGroup;
  */
 public class MeetNavneetFragment extends Fragment {
 
-    public MeetNavneetFragment() {
-        // Required empty public constructor
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_meet_navneet, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 }
