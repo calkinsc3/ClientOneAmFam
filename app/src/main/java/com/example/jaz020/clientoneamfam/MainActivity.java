eb431503059ae95b7657c3cd50cbdc08bcd3ab44
James Ziglinski <jaz020@amfam.com>
2015-07-29 15:56:06 -0500

    added james + levi portraits to drawable folder

--- app/src/main/java/com/example/jaz020/clientoneamfam/MainActivity.java
+++ app/src/main/java/com/example/jaz020/clientoneamfam/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends Activity {
         //enableUpAction();
 
         getActionBar().setHomeButtonEnabled(true);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(false);
 
 
         // Set the navigation drawer navigation
